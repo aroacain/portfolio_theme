80e0214e4e8e5cc64016aaed8036b6e46925edd5
Aroa Cain <aroacain@gmail.com>
2022-07-21 15:25:10 +0200

    Remove version number from css stylesheet

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function ourtheme_files() {
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     // Compiled js and css files
     wp_enqueue_script('main-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
-    wp_enqueue_style('our-main-styles-vendor', get_theme_file_uri('/build/index.css'));
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/build/index.css'), array(), null);
     }
 
 
