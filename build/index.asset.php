33439b8a4ea6fb4c80fac3615e7d1edf003db49f
Aroa Cain <aroacain@gmail.com>
2022-07-27 17:37:01 +0200

    add about me section

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'd954764844e94b8f7d61');
+<?php return array('dependencies' => array(), 'version' => '3dfc98260a6b381c4176');
--- page-homepage.php
+++ page-homepage.php
@@ -12,13 +12,32 @@
 
 <div class="navbar">
     <div class="navbar__menu">
-        <a href="#" class="navbar__element">About Me</a>
-        <a href="#" class="navbar__element">My Skills</a>
-        <a href="#" class="navbar__element">My Work</a>
-        <a href="#" class="navbar__element">Blog</a>
-        <a href="#" class="navbar__element">Contact</a>
+        <?php
+              wp_nav_menu(array(
+                'theme_location' => 'headerMenuLocation',
+              ));
+        ?>
     </div>
 </div>
+
+<section class="about utility__bb">
+    <div class="container">
+        <h1 class="section__heading">About Me</h1>
+        <div class="columns">
+            <div class="columns__33">
+                <img class="container__circle" src="<?php echo get_theme_file_uri('/assets/img/Aroa1.jpg')?>" alt="">
+            </div>
+            <div class="columns__66 columns__center-aligned">
+                <p class="section__par">Cum sociis natoque penatibus et magnis dis parturient. Curabitur blandit
+                    tempus ardua ridiculus sed magna. Salutantibus vitae elit libero, a pharetra augue. Lorem ipsum
+                    dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna
+                    aliqua. Me non paenitet nullum festiviorem excogitasse ad hoc. Ambitioni dedisse scripsisse
+                    iudicaretur. Unam incolunt Belgae, aliam Aquitani, tertiam. Morbi fringilla convallis sapien, id
+                    pulvinar odio volutpat. A communi observantia non est recedendum.</p>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php
     get_footer();
